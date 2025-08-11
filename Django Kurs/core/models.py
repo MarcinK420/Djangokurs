e860cbd67c3888c4ac12402f7d58bfe2ba1fe5b0
MarcinK420 <156346564+MarcinK420@users.noreply.github.com>
2025-08-11 15:01:33 +0200

    Add files via upload

--- Django Kurs/core/models.py
+++ Django Kurs/core/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Service(models.Model):
+    name = models.CharField(max_length=100)  # nazwa usługi
+    description = models.TextField(default="")         # opis
+    price = models.DecimalField(max_digits=6, decimal_places=2)  # cena
+
+    def __str__(self):
+        return self.name
--- Django Kurs/core/views.py
+++ Django Kurs/core/views.py
@@ -1,4 +1,30 @@
-from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from .forms import ServiceForm
+from .models import Service
+from datetime import datetime
 
 def home(request):
-    return HttpResponse("Witaj w mojej pierwszej aplikacji Django!")
+    uslugi = Service.objects.all()  # pobierz wszystkie usługi z bazy
+    context = {
+        'imie': 'Kamil',
+        'aktualny_czas': datetime.now(),
+        'uslugi': uslugi,
+        'now': datetime.now()
+    }
+    return render(request, 'core/home.html', context)
+
+def about(request):
+    context = {
+        'now': datetime.now()
+    }
+    return render(request, 'core/about.html', context)
+
+def add_service(request):
+    if request.method == 'POST':
+        form = ServiceForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')  # przekieruj na stronę główną po dodaniu
+    else:
+        form = ServiceForm()
+    return render(request, 'core/add_service.html', {'form': form})
